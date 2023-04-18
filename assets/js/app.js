a102cec71bcccc4a78df03b1b6001a7dfd456368
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-04-18 09:01:48 +0000

    add back to top button to all pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -77,3 +77,5 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+mybutton.addEventListener("click", topFunction);
